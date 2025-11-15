fc953bdb24b6a7c325d72cdcb4db05954a87f625
Artem <Actionartem@gmail.com>
2025-11-16 00:32:57 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // то, что у тебя уже было
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
 
-export default nextConfig
+  // нужно для GitHub Pages
+  output: 'export',                    // статический экспорт в /out
+  basePath: isProd ? '/TeleCode' : '', // имя репозитория
+  assetPrefix: isProd ? '/TeleCode/' : '',
+};
+
+export default nextConfig;
