8ccbcdff4fb7be8b8c729dd69db237799b027584
Artem <Actionartem@gmail.com>
2025-11-17 14:51:53 +0300

    Ensure favicons are configured

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,39 +14,58 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        rel: 'icon',
         url: '/favicon.ico',
       },
       {
+        rel: 'icon',
         url: '/favicon-16x16.png',
         sizes: '16x16',
         type: 'image/png',
       },
       {
+        rel: 'icon',
         url: '/favicon-32x32.png',
         sizes: '32x32',
         type: 'image/png',
       },
       {
+        rel: 'icon',
         url: '/favicon-48x48.png',
         sizes: '48x48',
         type: 'image/png',
       },
       {
+        rel: 'icon',
         url: '/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
+        rel: 'icon',
         url: '/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
+        rel: 'icon',
         url: '/tc-logo.svg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-touch-icon.png',
+    shortcut: [
+      {
+        rel: 'shortcut icon',
+        url: '/favicon.ico',
+      },
+    ],
+    apple: [
+      {
+        rel: 'apple-touch-icon',
+        url: '/apple-touch-icon.png',
+        sizes: '180x180',
+      },
+    ],
   },
 }
 
